f6050fde048a1116e2b8da06ca0ce9cf93d3676a
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-07-30 20:29:13 +0300

    add mobile menu

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector(".header-nav__btn");
+const menuIcon = document.querySelector(".nav-icon");
+const headerTop = document.querySelector(".header__top");
+
+menuBtn.addEventListener("click", () => {
+  menuIcon.classList.toggle("nav-icon--active");
+  headerTop.classList.toggle("header__top--mobile");
+  document.body.toggle("no-scroll");
+});
