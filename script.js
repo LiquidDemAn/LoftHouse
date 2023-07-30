6d5c3b2ea100678f3ed5c69ab3a44eb42022f1b6
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-07-30 22:16:05 +0300

    add adaptive for benefits section

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const headerTop = document.querySelector(".header__top");
 menuBtn.addEventListener("click", () => {
   menuIcon.classList.toggle("nav-icon--active");
   headerTop.classList.toggle("header__top--mobile");
-  document.body.toggle("no-scroll");
+  document.body.classList.toggle("no-scroll");
 });
