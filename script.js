3834f1c424d44c11f2474e0415ed761efcacb8bd
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-08-03 20:24:00 +0300

    add phone mask

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ menuBtn.addEventListener("click", () => {
   headerTop.classList.toggle("header__top--mobile");
   document.body.classList.toggle("no-scroll");
 });
+
+// Phone Mask
+
+mask("[data-tel-input]");
+
+const phoneInputs = document.querySelectorAll("[data-tel-input]");
+
+phoneInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.value === "+") {
+      input.value = "";
+    }
+  });
+  input.addEventListener("blur", () => {
+    if (input.value === "+") {
+      input.value = "";
+    }
+  });
+});
